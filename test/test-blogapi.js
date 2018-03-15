661b4a6b483d5615573c8cce09e627da9f4da7b9
Dan Rosen <drosen84@gmail.com>
2018-03-15 12:52:19 -0500

    added blog post generate function. added seed blog post function to test file

---
+++ test/test-blogapi.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
+
+// this makes the expect syntax available throughout
+// this module
+const expect = chai.expect;
+
+const {BlogPost} = require('../models');
+const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
+
+chai.use(chaiHttp);
+
+function seedBlogPostData() {
+  console.info('seeding blog post data');
+  const seedData = [];
+  
+  for (let i=1; i<=10; i++) {
+    seedData.push(generateBlogPostData());
+  }
+  // this will return a promise
+  return BlogPost.insertMany(seedData);
+}
+
+function generateBlogPostData() {
+  return {
+    author: {
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName()
+    },
+    title: faker.lorem.words,
+    content: faker.lorem.paragraph,
+    created: faker.date.recent(),
+  };
+}
