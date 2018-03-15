934fd0d28d222292c954abfe28b04da41902d0d2
Dan Rosen <drosen84@gmail.com>
2018-03-15 14:25:47 -0500

    added test server address into config file. worked through creating get endpoint tests. currently not working

--- test/test-blogapi.js
+++ test/test-blogapi.js
@@ -18,7 +18,6 @@ chai.use(chaiHttp);
 function seedBlogPostData() {
   console.info('seeding blog post data');
   const seedData = [];
-  
   for (let i=1; i<=10; i++) {
     seedData.push(generateBlogPostData());
   }
@@ -32,38 +31,84 @@ function generateBlogPostData() {
       firstName: faker.name.firstName(),
       lastName: faker.name.lastName()
     },
-    title: faker.lorem.words,
-    content: faker.lorem.paragraph,
-    created: faker.date.recent(),
+    title: faker.lorem.words(),
+    content: faker.lorem.paragraph(),
   };
 }
 
+// function tearDownDb() {
+//   console.warn('Deleting database');
+//   return mongoose.connection.dropDatabase();
+// }
+
 function tearDownDb() {
-  console.warn('Deleting database');
-  return mongoose.connection.dropDatabase();
+  return new Promise((resolve, reject) => {
+    console.warn('Deleting database');
+    mongoose.connection.dropDatabase()
+      .then(result => resolve(result))
+      .catch(err => reject(err));
+  });
 }
 
 describe('Blog Post API resource', function() {
 
-    before(function() {
-      return runServer(TEST_DATABASE_URL);
-    });
+  before(function() {
+    return runServer(TEST_DATABASE_URL);
+  });
   
-    beforeEach(function() {
-      return seedBlogPostData();
-    });
+  beforeEach(function() {
+    return seedBlogPostData();
+  });
   
-    afterEach(function() {
-      return tearDownDb();
-    });
+  afterEach(function() {
+    return tearDownDb();
+  });
   
-    after(function() {
-      return closeServer();
-    });
+  after(function() {
+    return closeServer();
+  });
 
-    describe('GET endpoint', function() {
-        it('should return all existing blog posts', function() {
-            let res;
-            return chai.request(app)
+  describe('GET endpoint', function() {
+    it('should return all existing blog posts', function() {
+      let res;
+      return chai.request(app)
+        .get('/posts')
+        .then(function(_res) {
+          res = _res;
+          expect(res).to.have.status(200);
+          expect(res.body.posts).to.have.length.of.at.least(1);
+          return BlogPost.count();
         })
-    })
+        .then(function(count) {
+          expect(res.body.posts).to.have.length.of(count);
+        });
+    });
+    it('should return blog posts with the correct fields', function() {
+      let resPost;
+      return chai.request(app)
+        .get('/posts')
+        .then(function(res) {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body.posts).to.be.a('array');
+          expect(res.body.posts).to.have.length.of.at.least(1);
+
+          res.body.posts.forEach(function(post) {
+            expect(post).to.be.a('object');
+            expect(post).to.include.keys('id', 'title', 'author', 'content');
+          });
+          resPost = res.body.posts[0];
+          return BlogPost.findById(resPost.id);
+        })
+        .then(function(post) {
+          expect(resPost.id).to.equal(post.id);
+          expect(resPost.title).to.equal(post.title);
+          expect(resPost.author).to.equal(post.author);
+          expect(resPost.content).to.equal(post.content);
+          
+        });
+      
+    });
+  });
+
+});
